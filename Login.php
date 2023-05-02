81c54192fe23f5dc4fd38f6d126bce124caca978
subraAntoine <antoine.subra1@gmail.com>
2023-05-02 08:59:39 +0200

    debut de la page login

---
+++ Login.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="login.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="login-page">
+        <div class="title-wrap">
+            <h1 class="login-title">
+                <span class="span-title">Omnes</span>
+                MySkill
+            </h1>
+        </div>
+
+        <div class="form-wrap">
+            <form action="" class="login-form" method="POST">
+                <input type="text" name="username" placeholder="Username" class="input-form">
+                <input type="password" name="password" placeholder="Password" class="input-form">
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </section>
+</body>
+</html>
