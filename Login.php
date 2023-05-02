78c71cf9ee952a1b84275d29eb5ec7bf5a1c7c30
subraAntoine <antoine.subra1@gmail.com>
2023-05-02 11:49:44 +0200

    page login

--- Login.php
+++ Login.php
@@ -6,24 +6,31 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="login.css">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@200;300;400;800&display=swap" rel="stylesheet">
+    <title>Login</title>
 </head>
+
 <body>
     <section class="login-page">
         <div class="title-wrap">
             <h1 class="login-title">
                 <span class="span-title">Omnes</span>
-                MySkill
+                MySkills
             </h1>
         </div>
 
         <div class="form-wrap">
             <form action="" class="login-form" method="POST">
-                <input type="text" name="username" placeholder="Username" class="input-form">
-                <input type="password" name="password" placeholder="Password" class="input-form">
-                <button type="submit">Login</button>
+                <div class="form-input">
+                    <input class="input" type="text" name="username" placeholder="Username" class="input-form">
+                    <input class="input" type="password" name="password" placeholder="Password" class="input-form">
+                </div>
+                <button class="form-btn" type="submit">Se connecter</button>
             </form>
         </div>
+            <a class="create-link" href="#">Créer un compte</a>
     </section>
 </body>
 </html>
