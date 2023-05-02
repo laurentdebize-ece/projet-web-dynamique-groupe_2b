88c2afb7f63c8692c0125c2e6028973c567976c6
subraAntoine <antoine.subra1@gmail.com>
2023-05-02 17:18:22 +0200

    accueil etudiant

---
+++ accueilEtudiant.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="accueilEtudiant.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@200;300;400;800&display=swap" rel="stylesheet">
+    <title>Accueil</title>
+</head>
+<body>
+    <div class="page-wrapper">
+        <section class="left-banner">
+
+            <h1 class="left-banner-title">
+                <span class="left-title-span">Omnes</span>
+                MySkills
+            </h1>
+            <div class="profile-img">
+                <div class="photo"></div>
+            </div>
+            <div class="user-info">
+                <h4 class="user-name">Antoine SUBRA</h4>
+                <h6 class="user-school">ECE Lyon</h6>
+                <h6 class="user-statut">Etudiant</h6>
+            </div>
+            <ul class="menu-list">
+                <li class="menu-item">
+                    <a href="" class="menu-link">Mes competences</a>
+                </li>
+                <li class="menu-item">
+                    <a href="" class="menu-link">Mes matières</a>
+                </li>
+                <li class="menu-item">
+                    <a href="" class="menu-link">Compétences transverses</a>
+                </li>
+                <li class="menu-item">
+                    <a href="" class="menu-link">Toutes les compétences</a>
+                </li>
+                <li class="menu-item">
+                    <a href="" class="menu-link">Mon compte</a>
+                </li>
+            </ul>
+            <div class="menu-icon"></div>
+        </section>
+
+
+        <section class="dashboard">
+
+        </section>
+</div>
+
+</body>
+</html>
