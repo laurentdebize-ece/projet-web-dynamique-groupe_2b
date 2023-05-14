8fe777a47380d1657be98cc91d7600ee2cb168f2
subraAntoine <antoine.subra1@gmail.com>
2023-05-14 09:11:46 +0200

    save

--- Prof/validationProf.php
+++ Prof/validationProf.php
@@ -1,5 +1,17 @@
 <?php
+    session_start();
+    require_once '../bdd.php';
 
+    $idUtilisateur = $_SESSION['idUtilisateur'];
+
+    $requetesCompetencesValidation = $bdd->prepare("SELECT Competence.nomCompetence, Eleve.nomEleve FROM Competence INNER JOIN Note ON Competence.idCompetence = Note.idCompetence INNER JOIN Matiere ON Matiere.idMatiere = Note.idMatiere INNER JOIN Eleve ON Eleve.idEleve = Note.idEleve WHERE Matiere.idProf = :idProf AND Note.Validation = 1");
+    $requetesCompetencesValidation->bindParam(':idProf', $_SESSION['idProfesseur']);
+    $requetesCompetencesValidation->execute();
+
+    $competencesValidation = $requetesCompetencesValidation->fetchAll(PDO::FETCH_ASSOC);
+
+
+    print_r($competencesValidation);
 ?>
 
 <!DOCTYPE html>
