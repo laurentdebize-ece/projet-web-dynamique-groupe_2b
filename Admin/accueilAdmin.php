7bcf3ccb84c234695f1a04277661d3e08be70ff2
PaulBusetta <92506275+PaulBusetta@users.noreply.github.com>
2023-05-19 16:53:26 +0200

    ConflitAccueil

--- Admin/accueilAdmin.php
+++ Admin/accueilAdmin.php
@@ -30,13 +30,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Admin</title>
-</head>
-<body>
-    <h1>Hello admin</h1>
-    <nav><a href = "ajoutEleve.php"> Ajouter une eleve</a></nav>
-=======
     <link rel="stylesheet" href="accueilAdministrateur.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
