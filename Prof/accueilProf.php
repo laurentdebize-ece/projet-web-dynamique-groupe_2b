9325abaf81abfd410338b5693dd3e9f77861ce71
subraAntoine <antoine.subra1@gmail.com>
2023-05-23 09:51:31 +0200

    logout prof

--- Prof/accueilProf.php
+++ Prof/accueilProf.php
@@ -29,9 +29,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@200;300;400;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
     <a href="./validationProf.php">Validez les competences de mes eleves</a>
+    <div class="menu-icon">
+                <a href="../traitementLogout.php" class="icon-link"><i class="fa-solid fa-right-from-bracket"></i> </a>
+                        
+            </div>
 </body>
 </html>
